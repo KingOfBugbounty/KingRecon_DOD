25435a44717673bbee64c63542c9eaae956ffb08
KingOfTips <kingofbugbounty@gmail.com>
2021-08-18 15:19:00 -0300

    Delete telegram.sh

--- boot/telegram.sh
+++
@@ -1,1 +0,0 @@
-curl -F document=@"~/PENTESTER/bugbounty/defense/boot/domain1.txt" https://api.telegram.org/bot1587913736:AAFRSGoprsUpy1HLjFPfoHQKxs7U_yYYbxk/sendDocument?chat_id=215999042 ; rm -rf domain1.txt
