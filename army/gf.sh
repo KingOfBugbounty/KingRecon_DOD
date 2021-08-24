9b2c0f7633bb8db725b37bca9f3e4a8c14b4b943
KingOfTips <kingofbugbounty@gmail.com>
2021-08-24 00:46:15 -0300

    New Recon

--- army/gf.sh
+++
@@ -1,4 +0,0 @@
-mkdir ~/.gf
-git clone https://github.com/1ndianl33t/Gf-Patterns
-cd Gf-Patterns && mv *.json ~/.gf
-source /root/.bashrc
--- disa/dod/DoD/Full/README.md
+++
@@ -1,3 +0,0 @@
-# DoD
-Domains recons in the Department of Defense.
-# DoD_Recon
